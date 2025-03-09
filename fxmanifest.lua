ddf2fc858d2233d997190b4f72ac100a89dcd225
CristianCiubancan <balooprogames@gmail.com>
2025-03-09 14:55:10 +0200

    asdasd

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,14 @@
 fx_version 'cerulean'
 game 'gta5'
 
+ui_page 'dist/web/index.html'
+
+files {
+  'dist/web/index.html',
+  'dist/web/**/*.js',
+  'dist/web/**/*.css'
+}
+
 client_script 'dist/client/bundle.client.js'
 server_script 'dist/server/bundle.server.js'
 
-ui_page 'dist/index.html'
-
-files {
-  'dist/**/*'
-}
